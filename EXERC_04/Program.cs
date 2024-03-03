f3a7665a593fb3a8a7a1763ed6135d21bcd53efb
LuizFAraujo <luizfaraujo.eng@gmail.com>
2024-03-03 13:31:41 -0300

    Atividade 04

---
+++ EXERC_04/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+Criar um programa que escreva os números de 1 a 1000 na tela.
+*/
+
+namespace EXERC_04;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Clear();
+
+        Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+        Console.WriteLine("                    EXIBIR NÚMEROS                   ");
+        Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+        Console.WriteLine("\n");
+
+        for (int i = 0; i < 1000; i++)
+        { Console.WriteLine($"  • {i + 1}"); }
+
+        Console.WriteLine("\n");
+        Console.WriteLine("Pressione uma tecla para sair...");
+        Console.ReadKey();
+    }
+}
